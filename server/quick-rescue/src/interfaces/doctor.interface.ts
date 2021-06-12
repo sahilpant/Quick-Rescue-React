a0b1263eb0dec4e4fc6ca528af528c873e8e2b54
sahil pant <sahilpant16@gmail.com>
2021-06-12 15:44:24 +0530

    Added Video Call

--- server/quick-rescue/src/interfaces/doctor.interface.ts
+++ server/quick-rescue/src/interfaces/doctor.interface.ts
@@ -2,6 +2,7 @@ import * as mongoose from 'mongoose'
 export interface Doctor extends mongoose.Document{
     firstname:string,
     lastname:string,
+    speciality:string
     email:string,
     password:string,
 }
