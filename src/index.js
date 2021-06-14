d31b9ca373435f277dac69a753cb55f3f8d41608
sahil pant <sahilpant16@gmail.com>
2021-06-14 10:31:54 +0530

    chnages

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import axios from 'axios';
-
-axios.defaults.headers.common['Authorization'] = 'Bearer' + localStorage.getItem('token');
 
 ReactDOM.render(
   <React.StrictMode>
--- t.mjs
+++
@@ -1,9 +0,0 @@
-import axios from "axios"
-
-axios
-  .get("http://65.2.73.180:3000/userser/user/appointment", {
-    params: {
-      email: "sahilpant16@gmail.com",
-    },
-  })
-  .then((x) => console.log(x.data))
