35f4f26e6b2bccb11fd7a1ccbbc3d8df8a0573be
sahil pant <sahilpant16@gmail.com>
2021-06-14 11:16:08 +0530

    videocall-backend

---
+++ server.js
@@ -0,0 +1,38 @@
+const { deepEqual } = require("assert")
+const express = require("express")
+const http = require("http")
+const app = express()
+const server = http.createServer(app)
+const io = require("socket.io")(server, {
+	cors: {
+		origin: "http://localhost:3000",
+		methods: [ "GET", "POST" ]
+	}
+})
+
+const users = {}
+
+io.on("connection", (socket) => {
+	if(!users[socket.id]) {
+		users[socket.id] = socket.id;
+	}
+
+	socket.emit("me", socket.id)
+	console.log(users);
+
+	socket.on("disconnect", () => {
+		socket.broadcast.emit("callEnded")
+		delete(users[socket.id])
+		console.log(users)
+	})
+
+	socket.on("callUser", (data) => {
+		io.to(data.userToCall).emit("callUser", { signal: data.signalData, from: data.from, name: data.name })
+	})
+
+	socket.on("answerCall", (data) => {
+		io.to(data.to).emit("callAccepted", data.signal)
+	})
+})
+
+server.listen(5000, () => console.log("server is running on port 5000"))
